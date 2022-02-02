fa2c24bee6cccadaf61670d32118c071a2639c0e
gauravCHOUDHARY <98762768+cozzmic@users.noreply.github.com>
2022-02-02 21:53:38 +0530

    Delete 1HelloWorld.cpp

--- 1HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main(){
-    cout<<"Hello guys welcome to my world!\n"
-    "This is my first ever program.\n"
-    "Let soar heights together.......";
-    return 0;
-}
